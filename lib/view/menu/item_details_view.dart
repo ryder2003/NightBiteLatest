314c2a0e7dad97a166323d11692d696b63f51083
aarav0180 <aarav.18o2005@gmail.com>
2024-11-19 02:20:02 +0530

    location

--- lib/view/menu/item_details_view.dart
+++ lib/view/menu/item_details_view.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart' show FieldValue, FirebaseFirestore;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
